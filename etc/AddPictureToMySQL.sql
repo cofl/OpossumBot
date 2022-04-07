951c161c6c698977114d9f07dd8fdae3a0bc3fd6
Gabriella Agathon <gjstimac@mtu.edu>
2022-04-07 18:22:41 +0000

    Updated comments + readme

--- etc/AddPictureToMySQL.sql
+++ etc/AddPictureToMySQL.sql
@@ -1,3 +1,7 @@
+/*
+    Sample file to load actual files into the table. This is the general form of *loading*
+    files into the table that could (in theory) be supported from the Discord-side
+*/
 USE OpossumBot;
 DESC OpossumPictures;
 INSERT INTO OpossumPictures VALUES(1, LOAD_FILE('/possums/1.png'), "empty alt text");
