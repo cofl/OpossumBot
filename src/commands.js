924349d35c48ee67a49a010593025c302b42351c
Isaiah Mattson <isaiahmattson@outlook.com>
2022-04-21 22:21:34 -0400

    UNTESTED rudimentary if statement commands.

--- src/commands.js
+++ src/commands.js
@@ -46,7 +46,7 @@ async function userCommands(msg, args){
      * alt text, sends the message
      */
     if(args[0] === "!possum"){
-        
+        msg.reply("Sorry, this feature isn't implemented yet!");
     }
     
     //TODO both stroll and crypto need to be added to the db as blobs
@@ -55,22 +55,34 @@ async function userCommands(msg, args){
      * !stroll
      * https://cdn.discordapp.com/attachments/743621304246206494/937426290296881163/video0-5_1.mov
      */
+    if(args[0] === "!stroll"){
+        msg.reply("https://cdn.discordapp.com/attachments/743621304246206494/937426290296881163/video0-5_1.mov");
+    }
 
     /**
      * !crypto
      * https://cdn.discordapp.com/attachments/812580457719005206/926519160014516284/jpZySFGv8ZiWcX40.mp4
      */
+    if(args[0] === "!crypto"){
+        msg.reply("https://cdn.discordapp.com/attachments/812580457719005206/926519160014516284/jpZySFGv8ZiWcX40.mp4");
+    }
 
     /**
      * !vibe
      * https://cdn.discordapp.com/attachments/547164475535523890/735923050696015903/1593712826771.mp4
      */
+    if(args[0] === "!vibe"){
+        msg.reply("https://cdn.discordapp.com/attachments/547164475535523890/735923050696015903/1593712826771.mp4");
+    }
 
     /**
      * !wheel
      * "https://cdn.discordapp.com/attachments/161297309978591233/903331498294390794/video0_13.mp4"
      */
-
+    if(args[0] === "!wheel"){
+        msg.reply("https://cdn.discordapp.com/attachments/161297309978591233/903331498294390794/video0_13.mp4");
+    }
+    
     /**
      * !metar
      * could be a fun command to fetch data from the NWS/NOAA
